bf424bb76c62eb02a46ce0ccfe9b05f4980e4eb6
norisgiro <norisgiro@gmail.com>
2025-05-16 09:27:57 +0200

    Aufgabe 2

---
+++ 1_Einführung/Aufgabe2/DiscountCalculator.java
@@ -0,0 +1,9 @@
+package Aufgabe2;
+
+import java.util.List;
+
+public class DiscountCalculator {
+    public static double getDiscountPercentage(List<String> items) {
+        return items.stream().anyMatch(item -> item.equalsIgnoreCase("book")) ? 5.0 : 0.0;
+    }
+}
